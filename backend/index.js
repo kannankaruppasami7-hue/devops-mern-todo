3241f71c7a6cf1706779f7ef9813fd36bcd6f5f1
Kannan <kannankaruppasami7@gmail.com>
2026-04-10 11:09:14 +0530

    Fix ci mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -9,20 +9,20 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// mongoose.connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// }).then(() => console.log('MongoDB connected'))
-//   .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
 
 
-  const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/testdb";
+// const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/testdb";
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+// mongoose.connect(MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error(err));
 
